0fd193918c73ec357b82a65c5ff06f1af84f9b0b
AlbertDygov <albertdygov@tuta.com>
2025-09-09 11:24:30 +0200

    änderung von 09.09 und 10.09

--- js/script.js
+++ js/script.js
@@ -227,3 +227,16 @@ window.addEventListener("resize", () => {
 });
 
 requestAnimationFrame(animate);
+
+
+//#Scroll EVENT 
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
